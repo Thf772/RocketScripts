9f1b611c6cadbcc4b37144a5988fe5a2cc77d4e8
Nathan/Eilisha Shiraini <neshiraini@heptacle.fr>
2018-01-29 23:57:23 +0100

    Updated metadata

--- MasterRaceScript.js
+++ MasterRaceScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         RocketScripts
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://chat.clubelek.fr/*
